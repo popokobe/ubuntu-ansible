6cafb81a2693606f82fd913dc12abf41d36cc880
Yuki Nagase <y-nagase@y-nagase-MBA.local>
2024-08-10 17:00:10 +0900

    Changed comments in each file from Japanese to English

--- ubuntu-c/deploy_ssh_keys.sh
+++ ubuntu-c/deploy_ssh_keys.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# SSHキーの生成
+# Generate ssh key
 ssh-keygen -t ed25519 -f /root/.ssh/id_ed25519 -q -C "" -N ""
 
-# SSHキーのコピー
+# Copy public ssh key to the target host
 sshpass -p "password" ssh-copy-id -i /root/.ssh/id_ed25519.pub -o StrictHostKeyChecking=no root@ubuntu-t1
 
-# パスワード認証を無効にするためのコマンドをターゲットノードに送信し、sshdを再起動
-# service restart sshするとコンテナが落ちるので、kill -HUPしている
+# Connect to target host, disable ssh connection with password authentication, and restart sshd
+# "service restart ssh" will cause the container to crash, running "kill -HUP" command.
 ssh root@ubuntu-t1 -p 22  << 'EOF'
 sed -i 's/^PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 kill -HUP $(pgrep -x sshd)
 EOF
 
-# コンテナが終了しないように無限ループ
+# Avoid container termination
 tail -f /dev/null
